c48c7ba4a7f966072fa93d753f7ceb38eb7e2700
Ángel García Menéndez <flecktarn121@protonmail.com>
2019-11-06 14:41:20 +0100

    KML generated (not yet tested)

--- practice3/Exercise2/task3/kml.py
+++ practice3/Exercise2/task3/kml.py
@@ -27,6 +27,14 @@ def parse_XML(filename):
         exit(-1) 
 
 def get_routes(dom):
+    ''' Given the root of the XML, return a dictionary with the routes
+
+        Args:
+            dom (ElementTree): the root of the XML
+
+        Returns:
+            dict(route name (str), list of milestones (Milestone)): a dictionary with the route name
+            and a list of Milestone objects'''
     routes = {}
     for route in dom.findall("ruta"):
         route_name = route.get("nombre")
@@ -34,7 +42,6 @@ def get_routes(dom):
         milestones = route.find("milestones").findall("milestone")
         for milestone in milestones:
             name = milestone.get("name")
-            print(name)
             description = milestone.find("descripcion").text
             latitude = milestone.find("coordenadas").get("latitude")
             longitude = milestone.find("coordenadas").get("longitude")
@@ -43,14 +50,55 @@ def get_routes(dom):
     return routes
 
 def save_to_kml(routes):
-    kml=simplekml.Kml()
+    ''' Takes the dictionary of routes and milestones and parses it into a KML
+
+        Args:
+            routes (dict): the dictionary of routes and lists of milestones
+    '''
+    root = ET.Element("kml", attrib={"xmlns":"http://www.opengis.net/kml/2.2"})
+    # Se the name tag
+    document = generate_document(root)
 
     for route in routes.keys():
-        for milestone in routes[route]:
-            kml.newpoint(name=milestone.name,description=milestone.description, coords=[(milestone.longitude, milestone.latitude, milestone.altitude)])
-        break
-    
-    kml.save('rutas.kml')
+        generate_placemark(route, routes, document)
+    ET.ElementTree(root).write("rutas.kml", encoding="utf-8", xml_declaration=True)
+
+def generate_placemark(route, routes, document):
+    # Writes the coordinates for the route
+
+    placemark = ET.Element("Placemark")
+    placemark.append(ET.Element("name"))
+    placemark.find("name").text = route
+    placemark.append(ET.Element("description"))
+    linestring = ET.Element("LineString")
+    placemark.append(linestring)
+    linestring.append(ET.Element("extrude"))
+    linestring.find("extrude").text = "1"
+    linestring.append(ET.Element("tessellate"))
+    linestring.find("tessellate").text = "1"
+    linestring.append(ET.Element("altitudeMode"))
+    linestring.find("altitudeMode").text = "relativeToSeaFloor"
+    coordinates = ""
+    for milestone in routes[route]:
+        lon = str(milestone.longitude)
+        lat = str(milestone.latitude)
+        alt = str(milestone.altitude)
+        coordinates = coordinates + (lon + "," + lat + "," + alt + "\n")
+    linestring.append(ET.Element("coordinates"))
+    linestring.find("coordinates").text = coordinates
+    document.append(placemark)
+
+
+def generate_document(root):
+    # Creaes the document node and some of its children
+    document = ET.Element("Document")
+    root.append(document)
+    document.append(ET.Element("name"))
+    document.find("name").text = "Rutas"
+    document.append(ET.Element("description"))
+    document.find("description").text = "Diversas rutas turisticas"
+    return document
+
 
 class Milestone:
 
