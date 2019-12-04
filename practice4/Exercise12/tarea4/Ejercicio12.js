780faeab3c34efd9fbd6d213600abb925f7c7d17
Ángel García Menéndez <UO258654@uniovi.es>
2019-12-04 11:47:06 +0100

    Dynamic maps finally working

--- practice4/Exercise12/tarea4/Ejercicio12.js
+++ practice4/Exercise12/tarea4/Ejercicio12.js
@@ -20,11 +20,9 @@ class MapManager{
     }
 
     initializeMap(){
-        window.addEventListener("load", function() {
             var oviedo = {lat: 43.3672702, lng: -5.8502461};
             var mapaOviedo = new google.maps.Map(document.getElementById('map-canvas'),{zoom: 8,center:oviedo});
             var marcador = new google.maps.Marker({position:oviedo,map:mapaOviedo});
-        });
     }
 
 }
