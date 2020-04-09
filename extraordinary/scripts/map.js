2e040665c578c0c5762a48dcf4037b4fbe9f3bf4
Ángel García Menéndez <UO258654@uniovi.es>
2020-04-09 16:40:35 +0200

    Google maps implementation

--- extraordinary/scripts/map.js
+++ extraordinary/scripts/map.js
@@ -1,50 +1,40 @@
-class MapGenerator {
-    getUserPosition() {
-        if(navigator.geolocation){
-            window.navigator.geolocation.getCurrentPosition(this.display);
-        } else {
-            alert("Su navegador no soporta geolocalización.")
-        }
-    }
-
-    generateDirectionRequest(origin, travelMode){
-        let request = {
-            origin: {lat: origin.coords.latitude, lng: origin.coords.longitude},
-            destination: {lat: 43.389139, lng: -5659215},
-            travelMode: travelMode
-        };
-
-        return request;
-    }
+class MapManager{
 
     initializeMap(){
         var directionsService = new google.maps.DirectionsService();
         var directionsRenderer = new google.maps.DirectionsRenderer();
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
             zoom: 7,
-            center: {lat: 43.389139, lng: -5659215}
+            center: {lat: 43.389139, lng: -5.659215}
         });
         directionsRenderer.setMap(map);
+            window.navigator.geolocation.getCurrentPosition(function(position) {
+                let travelMode = "DRIVING";
+                let request = {
+                    origin: {lat: position.coords.latitude, lng: position.coords.longitude},
+                    destination: {lat: 43.389139, lng: -5.659215},
+                    travelMode: travelMode
+                };
+                directionsService.route(request, function(response, isSuccess){
+                    if (isSuccess === "OK") {
+                        directionsRenderer.setDirections(response);
+                    } else {
+                        window.alert("Hubo un problema al cargar la ruta al IES Rio Nora.")
+                    }
+                });
+            },
+                function(err) {
+                    window.alert(err.message);
+                });
     }
 
     generateRoute(){
-        let start = getUserPosition();
-        let travelMode = "DRIVING";
-        request = generateDirectionRequest(start, travelMode);
-        directionsService.route(request, function(response, isSuccess){
-            if (isSuccess === "OK") {
-                directionsRenderer.setDirections(response);
-            } else {
-                window.alert("Hubo un problema al cargar la ruta al IES Rio Nora.")
-            }
-        });
+        let directionsService = new google.maps.DirectionsService();
+        let directionsRenderer = new google.maps.DirectionsRenderer();
     }
 
 }
 
 
 //The variable needs to be declared outside. JS, i hate you so much
-var mapGenerator;
-window.addEventListener("load", function() {
-    mapGenerator = new MapGenerator();
-});
+var mm = new MapManager();
