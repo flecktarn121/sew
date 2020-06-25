72190435c6ea01edb91fbe7bcf63314e64faa9a0
Ángel García Menéndez <UO258654@uniovi.es>
2020-06-25 13:23:08 +0200

    Exam modification
    A button has been added so a route between Oviedo and the IES Rio Nora
    is shown

--- extraordinary/scripts/map.js
+++ extraordinary/scripts/map.js
@@ -32,6 +32,33 @@ class MapManager {
                 window.alert(err.message);
             });
     }
+
+    /**
+    * EXAM MODIFICATION
+    */
+    route2Oviedo() {
+        let directionsService = new google.maps.DirectionsService();
+        let directionsRenderer = new google.maps.DirectionsRenderer();
+        let map = new google.maps.Map(document.getElementById('map-canvas'), {
+            zoom: 7,
+            center: {lat: 43.389139, lng: -5.659215}
+        });
+
+        directionsRenderer.setMap(map);
+        let travelMode = "DRIVING";
+        let request = {
+            origin: {lat: 43.3619145, lng:  -5.8493887},
+            destination: {lat: 43.389139, lng: -5.659215},
+            travelMode: travelMode
+        };
+        directionsService.route(request, function(response, isSuccess){
+            if (isSuccess === "OK") {
+                directionsRenderer.setDirections(response);
+            } else {
+                window.alert("Hubo un problema al cargar la ruta al IES Rio Nora.")
+            }
+        });
+    }
 }
 
 let mm = new MapManager();
